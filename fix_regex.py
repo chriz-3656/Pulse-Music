b5fb22d91edfbd70d4bc3fe18a2daab660e18bf5
chriz-3656 <chriz-3656@users.noreply.github.com>
2026-09-23 15:57:30 -0400

    chore: Carefully clean up temp python scripts

--- fix_regex.py
+++
@@ -1,9 +0,0 @@
-with open("app/src/main/java/com/example/data/repository/MusicRepositoryImpl.kt", "r") as f:
-    lines = f.readlines()
-
-with open("app/src/main/java/com/example/data/repository/MusicRepositoryImpl.kt", "w") as f:
-    for line in lines:
-        if 'val jsonMatch = Regex' in line:
-            f.write('            val jsonMatch = Regex("<script id=\\"__NEXT_DATA__\\" type=\\"application/json\\">(.*?)</script>").find(html)\\n')
-        else:
-            f.write(line)
